0be1f12378a51fc5c025af9e570d6dc67dbf17fc
Luis R Zunniga S <admin@ecoquintas.com>
2023-05-17 09:37:02 -0600

    Creo la vista del login

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+  Alert,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  loginPressed = () => {
+    Alert.alert("Completed Login!");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/background.png")}
+      style={styles.background}
+    >
+      <View>
+        <Image
+          source={require("./assets/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        ></Image>
+        <Text style={styles.text}>En donde nace la Felicidad.</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Correo electrónico"
+            onChangeText={(email) => setEmail(email)}
+          ></TextInput>
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Contraseña"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          ></TextInput>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot_button}>Olvidé mi contraseña</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.loginButton} onPress={loginPressed}>
+          <Text style={styles.loginButtonText}>Iniciar sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    width: "100%",
+    height: "100%",
+  },
+  logo: {
+    width: 280,
+    height: 280,
+    marginLeft: "15%",
+    marginTop: "10%",
+  },
+  text: {
+    color: "white",
+    marginTop: "-5%",
+    marginLeft: "15%",
+    marginBottom: 50,
+  },
+  loginButton: {
+    width: "75%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 40,
+    marginBottom: 20,
+    borderRadius: 25,
+  },
+  loginButtonText: {
+    backgroundColor: "#168039",
+    color: "white",
+    borderRadius: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 27,
+    paddingVertical: 10,
+  },
+  inputView: {
+    marginTop: 20,
+    backgroundColor: "white",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    alignItems: "center",
+    marginLeft: 50,
+    marginBottom: 10,
+  },
+  TextInput: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 40,
+    padding: 10,
+  },
+  forgot_button: {
+    alignSelf: "center",
+    height: 20,
+    marginTop: 15,
+    marginBottom: 30,
   },
 });
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Main() {
+  return (
+    <View>
+        <Text> Este es el componente Main</Text>
+    </View>
+  )
+}
